5f25d9d0f4e6269660b461261ea4eb97d634364d
Ben Jones <ben.jones@myport.ac.uk>
2020-05-02 15:23:02 +0100

    Now including config_template.py

---
+++ data/config_template.py
@@ -0,0 +1,16 @@
+VERBOSE = True
+
+# Project details
+
+CLIENT = ""  # Add a client here for clientName filter
+
+# CREDENTIALS
+API_KEY = {"X-Api-Key": "______________"}
+WORKSPACE_ID = "________________"
+USER_ID = (
+    "___________________________"
+)  # recalled this - see docs for how to obtain userid from active
+
+# ENDPOINTS
+ENDPOINT = "https://api.clockify.me/api/v1"
+
